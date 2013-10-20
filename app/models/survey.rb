a15875e103f9cb2e2dd5e56fd7e942e8a8543c4c
Fabiana Anjos Campelo <fabyanjos@gmail.com>
2013-10-20 14:06:01 +0200

    error message layout

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,6 +1,7 @@
 class Survey < ActiveRecord::Base
+  validates :title, presence: true
   belongs_to :user
   has_many :questions, :dependent => :destroy
-  attr_accessible :description, :style, :title, :questions_attributes 
+  attr_accessible :description, :style, :title, :questions_attributes, :end_date, :url, :end_qty_votes, :closed
   accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:title].blank? }, :allow_destroy => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131019131704) do
+ActiveRecord::Schema.define(:version => 20131020101404) do
 
   create_table "orders", :force => true do |t|
     t.integer  "vote_id"
@@ -34,8 +34,12 @@ ActiveRecord::Schema.define(:version => 20131019131704) do
     t.text     "description"
     t.string   "style"
     t.integer  "user_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.datetime "end_date"
+    t.string   "url"
+    t.integer  "end_qty_votes"
+    t.boolean  "closed"
   end
 
   create_table "users", :force => true do |t|
